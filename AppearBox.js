205f98cebe151ac635d5526ee22806611e96f1ba
공대여자 <mins01.lycos.co.kr@gmail.com>
2019-05-08 17:28:17 +0900

    first

---
+++ AppearBox.js
@@ -0,0 +1,46 @@
+var AppearBox = (function(){
+	var AppearBox = function(target){
+		this.target = target;
+		this.tc = new TimerChain();
+	};
+	var prt = AppearBox.prototype;
+	prt.target = null;
+	prt.tc = null;
+	prt.contentHtml = function(html){
+		$(this.target).find('.appear-box-content').html(html)
+		return this;
+	}
+	prt.contentText = function(text){
+		$(this.target).find('.appear-box-content').text(text)
+		return this;
+	}
+	prt.show = function(delay){
+		if(delay==null) delay = 0;
+		var thisC = this;
+		
+		this.tc.push(function(){
+			$(thisC.target).addClass('on');
+			$(thisC.target).addClass('animated bounceIn');	
+		},delay)
+		.push(function(){
+			$(thisC.target).removeClass('animated bounceIn');	
+		},1000).start()
+		
+		return this
+	}
+	prt.hide = function(delay){
+		if(delay==null) delay = 0;
+		var thisC = this;
+		this.tc.push(function(){
+			$(thisC.target).addClass('animated bounceOut');	
+		},delay)
+		.push(function(){
+			$(thisC.target).removeClass('animated bounceOut');	
+			$(thisC.target).removeClass('on');
+
+		},1000).start();
+		return this
+	}
+	
+	return AppearBox;
+})()
